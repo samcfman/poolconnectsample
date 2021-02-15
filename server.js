f984fce4360d411b7c37e4653eb7716d041339b9
samcfman <sman@sman-ltm1.internal.salesforce.com>
2021-02-15 10:56:32 +0800

    v1

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const app = express();
 //const port = 3000;
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
+const { Pool } = require ('pg');
+const pool = new Pool ({
+	//connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.HEROKU_POSTGRESQL_AMBER_URL,
+	
+	ssl: {
+		rejectUnauthorized: false
+	  },
+	//max: 500  	
+
+});
+
+
+//module.exports.client = client;
+module.exports.client = poolConnectionClient;
+
+
 
 //app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -21,6 +38,7 @@ var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
 });
+
 
 // ** TUNE **
 /*var poolConnectionClient;
