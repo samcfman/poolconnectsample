c1a09d12ba80698672aa30814d587ae747e1b2b0
samcfman <sman@sman-ltm1.internal.salesforce.com>
2021-02-15 09:54:48 +0800

    v1

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ function handleError(res, reason, message, code) {
 const { Client, Pool } = require ('pg');
 const { release } = require("os");
 
-/*const client = new Client({
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
@@ -59,7 +59,7 @@ client.connect(err => {
     console.log('connected')
   }
 });
-*/
+
 const pool = new Pool ({
 	//connectionString: process.env.DATABASE_URL,
 	connectionString: process.env.HEROKU_POSTGRESQL_AMBER_URL,
