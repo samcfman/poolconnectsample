4a0bc1e5ac3e88fb76ecf2a0f17724e8c778b138
samcfman <sman@sman-ltm1.internal.salesforce.com>
2021-02-15 10:36:21 +0800

    v1

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+
+
 // ** TUNE **
 /*var poolConnectionClient;
 
